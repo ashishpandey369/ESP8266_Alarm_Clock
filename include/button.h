621f97e04370013ff842006e889aaa451b460a5a
Ashish Pandey <ap965599@gmail.com>
2026-09-02 01:32:30 +0530

    Add button hold and repeat support

--- include/button.h
+++ include/button.h
@@ -8,10 +8,13 @@ public:
 
     void begin();
     bool pressed();
+    bool repeatPressed(uint32_t initialDelayMs = 500, uint32_t repeatIntervalMs = 100);
 
 private:
     uint8_t pin_;
     bool lastState_;
     bool stableState_;
     uint32_t lastChangeMs_;
+    uint32_t pressStartMs_;
+    uint32_t lastRepeatMs_;
 };
