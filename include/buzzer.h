6339d3055d165ad7bdf48bc155a31c900b6bbf89
Ashish Pandey <ap965599@gmail.com>
2026-09-02 02:00:23 +0530

    Add audible alarm tone control

--- include/buzzer.h
+++ include/buzzer.h
@@ -10,10 +10,15 @@ public:
     void on();
     void off();
     void beep(uint16_t durationMs);
+    void startAlarm();
+    void updateAlarm();
+    void stopAlarm();
     bool isActive() const;
 
 private:
     uint8_t pin_;
     bool active_;
     uint32_t offAtMs_;
+    uint32_t lastToneToggleMs_;
+    bool toneOn_;
 };
