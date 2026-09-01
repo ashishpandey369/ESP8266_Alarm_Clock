2d9ccad8ec26199d7967d54b6ca282153f037475
Ashish Pandey <ap965599@gmail.com>
2026-09-02 01:47:07 +0530

    Add modular alarm manager

---
+++ include/alarm.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <Arduino.h>
+
+class AlarmManager {
+public:
+    AlarmManager();
+
+    void begin(uint8_t defaultHour, uint8_t defaultMinute);
+    void setTime(uint8_t hour, uint8_t minute);
+    uint8_t getHour() const;
+    uint8_t getMinute() const;
+    void enable();
+    void disable();
+    bool isEnabled() const;
+    bool matches(uint8_t hour, uint8_t minute) const;
+
+private:
+    uint8_t hour_;
+    uint8_t minute_;
+    bool enabled_;
+};
