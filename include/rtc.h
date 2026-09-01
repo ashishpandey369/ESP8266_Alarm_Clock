5e5d7fe56dfa2d8eeca1ae3a61fd5b200dd9731f
Ashish Pandey <ap965599@gmail.com>
2026-09-02 01:37:34 +0530

    Implement DS1307 RTC interface

--- include/rtc.h
+++ include/rtc.h
@@ -1,15 +1,18 @@
 #pragma once
 
 #include <Arduino.h>
-
-// DS3231 RTC interface.
-// The RTC shares the I2C bus with the TM1637 only if the display is I2C-compatible;
-// TM1637 is a separate 2-wire protocol, so its pins remain independent.
+#include <RTClib.h>
 
 class RTCManager {
 public:
     bool begin();
     bool isRunning() const;
     bool hasValidTime() const;
+    bool getTime(uint8_t &hour, uint8_t &minute) const;
+    bool setTime(uint8_t hour, uint8_t minute);
     void printStatus(Stream &serial) const;
+
+private:
+    RTC_DS1307 rtc_;
+    bool initialized_ = false;
 };
